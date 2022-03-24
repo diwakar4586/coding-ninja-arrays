43d4504e863e2e48b577aa467dd706088ff8fdb2
diwakar4586 <diwakar0470.be21@chitkara.edu.in>
2022-03-24 21:31:38 +0530

    second_commit

--- arraysum.cpp
+++ arraysum.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
-    
-    return 0;
+    int n;
+    cin>>n;
+
+    int values[100000];
+    for(int i = 0;i<n;i++){
+        cin>>values[i];
+    }
+  
+
+    int sum = 0;
+
+    for(int i = 0;i<n;i++){
+        sum = sum +values[i];
+
+    }
+
+    cout<<sum;
 }
+
+
